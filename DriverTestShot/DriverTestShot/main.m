da5b44dcce7340962c2630ec936fbef02fcdf3f5
Eren <eren.shi@nextlabs.com>
2015-08-18 15:00:29 +0800

    init project to github

---
+++ DriverTestShot/DriverTestShot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DriverTestShot
+//
+//  Created by EShi on 8/18/15.
+//  Copyright (c) 2015 Eren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
