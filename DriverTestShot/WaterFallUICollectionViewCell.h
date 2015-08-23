0ab19eb12475696e68df157c0d2c5f1bd3fc45ab
ShiTeng <shiteng@ShiTengdeMacBook-Air.local>
2015-08-23 19:57:04 +0800

    Create the architecture of project

---
+++ DriverTestShot/WaterFallUICollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WaterFallUICollectionViewCell.h
+//  WaterFall
+//
+//  Created by JackXu on 15/5/8.
+//  Copyright (c) 2015年 BFMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaterFallUICollectionViewCell : UICollectionViewCell
+@property(nonatomic,strong) UIImage *image;
+@end
